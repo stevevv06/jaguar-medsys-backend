f3ab73550d26b8c01075c46696c47df46780dda1
Alfredo Steve Vasquez Villalta <avasquez@sitescorp.com>
2018-03-12 09:33:02 -0600

    adding template from start.spring.io

---
+++ src/test/java/sv/com/jaguarsoft/jaguarmedsysbackend/JaguarMedsysBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package sv.com.jaguarsoft.jaguarmedsysbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JaguarMedsysBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
