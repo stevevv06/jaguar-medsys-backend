5519eec45cc133082eaffeb683da8e2ccd4770a5
Alfredo Steve Vasquez Villalta <avasquez@sitescorp.com>
2018-03-16 11:05:49 -0600

    updates

--- src/main/java/sv/com/jaguarsoft/jaguarmedsys/jparepositories/PatientRepository.java
+++ src/main/java/sv/com/jaguarsoft/jaguarmedsys/jparepositories/PatientRepository.java
@@ -1,8 +1,12 @@
 package sv.com.jaguarsoft.jaguarmedsys.jparepositories;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import sv.com.jaguarsoft.jaguarmedsys.entities.Patient;
@@ -10,4 +14,7 @@ import sv.com.jaguarsoft.jaguarmedsys.entities.Patient;
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    @RestResource(path = "findByTitle", rel = "findByTitle")
+    public Page<Patient> findByTitleContainingIgnoreCase(@Param("title") String title, Pageable p);
 }
